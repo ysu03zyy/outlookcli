a0c3fff3c69b72aadfc6ecdf2a66c5fc3ba35364
zyy <461419290@qq.com>
2026-04-15 19:48:19 +0800

    chore(formula): update outlookcli v0.1.1 checksums

--- Formula/outlookcli.rb
+++ Formula/outlookcli.rb
@@ -13,25 +13,25 @@ class Outlookcli < Formula
   desc "Outlook mail and calendar via Microsoft Graph"
   homepage "https://github.com/ysu03zyy/outlookcli"
   license "MIT"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ysu03zyy/outlookcli/releases/download/v#{version}/outlookcli_#{version}_darwin_arm64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_ARM64_SHA256"
+      sha256 "701a20f68a3c4c24f74ae400b55a4c82a818ddbb7db3715c9b785122d05d9f55"
     else
       url "https://github.com/ysu03zyy/outlookcli/releases/download/v#{version}/outlookcli_#{version}_darwin_amd64.tar.gz"
-      sha256 "REPLACE_WITH_DARWIN_AMD64_SHA256"
+      sha256 "840bd5c484a3a80e683e0dd6f7bed9e192f16b7444fbe5040bf24804ecbd2b28"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/ysu03zyy/outlookcli/releases/download/v#{version}/outlookcli_#{version}_linux_arm64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_ARM64_SHA256"
+      sha256 "9c74afebb829deb689abc8eb6605e6415fbef662c1649206ff0cd74b8fce4184"
     else
       url "https://github.com/ysu03zyy/outlookcli/releases/download/v#{version}/outlookcli_#{version}_linux_amd64.tar.gz"
-      sha256 "REPLACE_WITH_LINUX_AMD64_SHA256"
+      sha256 "e891e4978d1ad99b1bb9d0a74f18e277d9f87a3841702d72d6ed7d212599dd90"
     end
   end
 
